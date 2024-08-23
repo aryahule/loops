9dea90d5ac0ec2134c3d54e16f5ec12a3eeb3459
aryahule <aryahule27@gmail.com>
2024-08-23 12:16:26 +0530

    Add files via upload

---
+++ evennumberprinting.cpp
@@ -0,0 +1,24 @@
+//Name:AryaHule
+//PRN:23070123029
+//AIM - To print even number using loops.
+#include <iostream>
+using namespace std;
+
+int main() {
+ for(int i=0;i<11;i++)
+{if(i%2==0)
+{cout << i<< endl;}}
+
+    return 0;
+}
+
+/*Output-
+0
+2
+4
+6
+8
+10
+
+
+*/
